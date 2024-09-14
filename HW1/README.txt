db4c3ba26aa7032268c472546751b7860643d75a
Mitchell Scott <mitchelltscott97@gmail.com>
2024-09-14 16:01:21 -0400

    Upload Documents

---
+++ HW1/README.txt
@@ -0,0 +1,7 @@
+/* THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT
+CONSULTING CODE WRITTEN BY OTHER STUDENTS
+OR LARGE LANGUAGE MODELS LIKE CHATGPT.
+Mitchell Scott
+
+I collaborated with the following classmates for this homework:
+<names of classmates>
---
+++ HW1/elastic.py
@@ -0,0 +1,21 @@
+
+def loss(x, y, beta, el, alpha):
+    return 0
+
+def grad_step(x, y, beta, el, alpha, eta):
+    return 0
+
+
+class ElasticNet:
+    def __init__(self, el, alpha, eta, batch, epoch):
+        # do something
+        return
+
+    def coef(self):
+        return 0
+
+    def train(self, x, y):
+        return 0
+
+    def predict(self, x):
+        return 0
