f3d6251ccd09d5561a54fcec047cc28940edc99c
Mitchell Scott <mitchelltscott97@gmail.com>
2024-10-02 09:38:45 -0400

    Updated q2.py

--- HW2/q2.py
+++ HW2/q2.py
@@ -1,14 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+# from matplotlib import pyplot
+# import matplotlib.pyplot as plt
+# import pandas as pd
 from sklearn import linear_model
 import sklearn.linear_model as skl
 from sklearn import preprocessing
-from sklearn import FunctionTransformer
+from sklearn.preprocessing import FunctionTransformer
 from sklearn import naive_bayes
 from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 
 def do_nothing(train, test):
@@ -30,9 +31,9 @@ def do_std(train, test):
 
 def do_log(train, test):
     # your code here
-    transformer = FunctionTransformer(np.log + 0.1, validate=True)
+    transformer = FunctionTransformer(np.log1p, validate=True)
     trainx = transformer.transform(train)
-    testx = transformer.transform(train)
+    testx = transformer.transform(test)
     return trainx, testx
 
 
@@ -49,17 +50,49 @@ def eval_nb(trainx, trainy, testx, testy):
     # your code here
     gnb = GaussianNB()
 
-    predy = gnb.fit(trainx, trainy).predict(testx)
+    # Predicting Data
+    y_train_pred = gnb.fit(trainx, trainy).predict(trainx)
+    y_test_pred = gnb.fit(trainx, trainy).predict(testx)
 
-    test-acc = ((testy == predy).sum())/ testx.shape[0]
+    # Calculating Accuracy
+    train_acc = ((trainy == y_train_pred).sum())/ trainx.shape[0]
+    test_acc = ((testy == y_test_pred).sum())/ testx.shape[0]
+
+    # Calculating AUC
+    fpr_train, tpr_train, thresholds = roc_curve(trainy, y_train_pred)
+    train_auc = auc(fpr_train, tpr_train)
+    fpr_test, tpr_test, thresholds = roc_curve(testy, y_test_pred)
+    test_auc = auc(fpr_test, tpr_test)
+
+    # Calculating Probability
+    test_prob = gnb.predict_proba(testx)
     return {"train-acc": train_acc, "train-auc": train_auc,
             "test-acc": test_acc, "test-auc": test_auc,
             "test-prob": test_prob}
 
 
 def eval_lr(trainx, trainy, testx, testy):
-    test_prob = np.zeros(textx.shape[0])
+    test_prob = np.zeros(testx.shape[0])
     # your code here
+    lr = LogisticRegression(random_state=19, penalty=None).fit(trainx, trainy)
+
+    y_train_pred = lr.predict(trainx)
+    y_test_pred = lr.predict(testy)
+    # cnf_mat_train = confusion_matrix(trainy, y_train_pred)
+    # cnf_mat_test = confusion_matrix(testy, y_test_pred)
+
+    # Calculating Accuracy
+    train_acc = ((trainy == y_train_pred).sum())/ trainx.shape[0]
+    test_acc = ((testy == y_test_pred).sum())/ testx.shape[0]
+
+    # Calculating AUC
+    fpr_train, tpr_train, thresholds = roc_curve(trainy, y_train_pred)
+    train_auc = auc(fpr_train, tpr_train)
+    fpr_test, tpr_test, thresholds = roc_curve(testy, y_test_pred)
+    test_auc = auc(fpr_test, tpr_test)
+
+    # Calculating Probability
+    test_prob = lr.predict_proba(testx)
     return {"train-acc": train_acc, "train-auc": train_auc,
             "test-acc": test_acc, "test-auc": test_auc,
             "test-prob": test_prob}
